9f13363563f20474043b975c14d2a10df5450c9c
faheem <faheem.bhatti@smart-systems-hub.de>
2025-07-22 12:58:34 +0200

    Update inside submodule

--- basyx.aasdiscoveryservice/basyx.aasdiscoveryservice.component/src/main/java/org/eclipse/digitaltwin/basyx/aasdiscoveryservice/component/AasDiscoveryServiceComponent.java
+++ basyx.aasdiscoveryservice/basyx.aasdiscoveryservice.component/src/main/java/org/eclipse/digitaltwin/basyx/aasdiscoveryservice/component/AasDiscoveryServiceComponent.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (C) 2023 the Eclipse BaSyx Authors
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -19,7 +19,7 @@
  * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
+ *
  * SPDX-License-Identifier: MIT
  ******************************************************************************/
 
@@ -33,13 +33,13 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 /**
  * Creates and starts the {@link AasDiscoveryService} off-shelf-component
- * 
+ *
  * @author schnicke, danish
  *
  */
-@SpringBootApplication(scanBasePackages = "org.eclipse.digitaltwin.basyx.aasdiscoveryservice")
+//@SpringBootApplication(scanBasePackages = "org.eclipse.digitaltwin.basyx.aasdiscoveryservice")
 public class AasDiscoveryServiceComponent {
-	public static void main(String[] args) {
-		SpringApplication.run(AasDiscoveryServiceComponent.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(AasDiscoveryServiceComponent.class, args);
+//	}
 }
